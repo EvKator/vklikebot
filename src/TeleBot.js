181639520cbbe78a148fe5dcfb58eb54fa6f63ce
EvKator <30655686+EvKator@users.noreply.github.com>
2018-04-26 13:35:47 +0300

    Update TeleBot.js

--- src/TeleBot.js
+++ src/TeleBot.js
@@ -1,3 +1,3 @@
 let TelegramBot = require('node-telegram-bot-api');
-let token = /*"591127104:AAHSbfiVlcfO3onzsHf-PcJl0YftTtbisA0";//*/"275206810:AAF8UOGALom--cuL5pkXhA_0_YJQ--JwlbQ";
+let token = "TOKEN";
 export default new TelegramBot(token, {polling: true});
